d782556ed083ef2f892e178848a17f8a59b0b842
DIUPAKLONG <126437661+DIUPAKLONG@users.noreply.github.com>
2023-03-13 11:08:42 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.forever(function () {
+    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Run)
+    if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 200)
+    } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.White) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.Black)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinRight, 218)
+    } else if (mbit_Robot.Line_Sensor(mbit_Robot.enPos.LeftState, mbit_Robot.enLineState.Black) && mbit_Robot.Line_Sensor(mbit_Robot.enPos.RightState, mbit_Robot.enLineState.White)) {
+        mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinLeft, 216)
+    } else {
+        mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+    }
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
